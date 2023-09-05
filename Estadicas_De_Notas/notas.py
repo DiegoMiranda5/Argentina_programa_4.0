2ae5fc28dc8938f28f3e0b389b0b46f07c11095f
DiegoMiranda5 <diegomiranda610@gmail.com>
2023-09-05 16:03:38 -0300

    Se agrego alguna comprobaciones para las notas y las salidas por consola

--- Estadicas_De_Notas/notas.py
+++ Estadicas_De_Notas/notas.py
@@ -5,12 +5,23 @@ cantNotasAprobadas = 0
 cantNotasPromocionados = 0
 
 while nota != 0:
-    nota = int(input("Ingrese una nota"))
-    cantNotas += 1
-    if nota < 4:
+    nota = int(input("Ingrese una nota -> "))
+    
+    if nota == 0:
+        print("Finalizando")
+    elif nota < 4:
         cantNotasAplazadas += 1
+        cantNotas += 1
+    elif nota >= 4 and nota <= 7:
+        cantNotasAprobadas += 1
+        cantNotas += 1
     elif nota > 7:
         cantNotasPromocionados += 1
-    else:
-        cantNotasAprobadas += 1 
+        cantNotas += 1
+    elif nota < 1 or nota > 10:
+        print("la nota ingresada es invalida")
     
+if cantNotas > 0:
+    print(f'Cantidad de aplazos:  {cantNotasAplazadas} ({round((cantNotasAplazadas*100)/cantNotas)}%)')
+    print(f'Cantidad de aprobados:  {cantNotasAprobadas} ({round((cantNotasAprobadas*100)/cantNotas)}%)')
+    print(f'Cantidad de promocionados:  {cantNotasPromocionados} ({round((cantNotasPromocionados*100)/cantNotas)}%)')
